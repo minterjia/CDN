f47a9ad5868ed817eac226884331c15383fe30ba
minterjia <61584074+minterjia@users.noreply.github.com>
2022-06-30 18:05:42 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function loadScript(src, callback) {
+        var script = document.createElement('script'),
+            head = document.getElementsByTagName('head')[0];
+        script.type = 'text/javascript';
+        script.charset = 'UTF-8';
+        script.src = src;
+        if (script.addEventListener) {
+            script.addEventListener('load', function () {
+                callback();
+            }, false);
+        } else if (script.attachEvent) {
+            script.attachEvent('onreadystatechange', function () {
+                var target = window.event.srcElement;
+                if (target.readyState == 'loaded') {
+                    callback();
+                }
+            });
+        }
+        head.appendChild(script);
+    }
+    function loadlive2d() {
+        if (document.body.clientWidth > 600) {
+            document.onreadystatechange = function () {
+                if (document.readyState == "complete") {
+                    loadScript('https://cdn.jsdelivr.net/gh/minterjia/CDN@2.0.0/js/pixi.min.js,https://cdn.jsdelivr.net/gh/minterjia/CDN@2.0.0/js/live2dcubismcore.min.js,https://cdn.jsdelivr.net/gh/minterjia/CDN@2.0.0/js/live2dcubismframework.min.js,https://cdn.jsdelivr.net/gh/minterjia/CDN@2.0.0/js/live2dcubismpixi.min.js,https://cdn.jsdelivr.net/gh/minterjia/CDN2.0.0/js/load.js',function(){
+                        loadModel();
+})
+                }
+            }
+        }
+    }
+loadlive2d()
---
+++ index2.js
@@ -0,0 +1,8 @@
+
+/**
+ * Minified by jsDelivr using Terser v5.3.5.
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+function loadScript(e,n){var t=document.createElement("script"),a=document.getElementsByTagName("head")[0];t.type="text/javascript",t.charset="UTF-8",t.src=e,t.addEventListener?t.addEventListener("load",(function(){n()}),!1):t.attachEvent&&t.attachEvent("onreadystatechange",(function(){"loaded"==window.event.srcElement.readyState&&n()})),a.appendChild(t)}function loadlive2d(){document.body.clientWidth>600&&(document.onreadystatechange=function(){"complete"==document.readyState&&loadScript("https://cdn.jsdelivr.net/gh/minterjia/CDN@2.0.0/js/loadModel.js",(function(){loadModel()}))})}loadlive2d();
+//# sourceMappingURL=/sm/e8d2b25053ed3e14bcf4ca020cd4d8b655de282403b4059e1627356b4f1a7cc7.map
